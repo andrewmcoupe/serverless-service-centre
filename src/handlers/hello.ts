fcff1ebb93115199cb5538e6379bc5e6207d44d1
Andy Coupe <andrewmcoupe@gmail.com>
2020-08-11 07:25:22 +0100

    initial commit

---
+++ src/handlers/hello.ts
@@ -0,0 +1,10 @@
+import { APIGatewayProxyHandler } from 'aws-lambda'
+
+const handler: APIGatewayProxyHandler = async () => {
+  return {
+    statusCode: 200,
+    body: 'Hello world!',
+  }
+}
+
+export default handler
