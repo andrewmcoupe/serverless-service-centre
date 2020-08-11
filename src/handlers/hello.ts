33ff1617146c317d6fcdb3afb7ad22dc83cea7e1
Andy Coupe <andrewmcoupe@gmail.com>
2020-08-11 07:40:35 +0100

    add test support and github actions

--- src/handlers/hello.ts
+++ src/handlers/hello.ts
@@ -1,10 +1,8 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
 
-const handler: APIGatewayProxyHandler = async () => {
+export const handler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
     body: 'Hello world!',
   }
 }
-
-export default handler
