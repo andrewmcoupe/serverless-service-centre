4966777e30a8e3c83fcbd3211c363623a913c81a
Andy Coupe <andrewmcoupe@gmail.com>
2020-08-15 13:25:16 +0100

    Edit lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  '*.{ts}': ['eslint --fix'],
+  'src/**/*.{ts}': ['eslint --fix'],
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
   '*.{ts,js,json,yml}': ['prettier --write'],
 }
