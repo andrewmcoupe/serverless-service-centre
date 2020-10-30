efb3c3fd0233d5652d1be4235de8b22721c1e0f1
Andy Coupe <andrewmcoupe@gmail.com>
2020-10-30 07:18:10 +0000

    Remove integ tests (#31)
    * allow customer history record to be created
    
    * Add tests for create history record
    
    * remove mock
    
    * add wait for expect
    
    * remove needless integ tests

---
+++ src/handlers/historyRecordCreate.ts
@@ -0,0 +1,50 @@
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { validate } from 'class-validator'
+import * as CustomerService from '../services/customerService'
+import { HistoryRecord } from '../domain/CustomerModel'
+
+export const handler: APIGatewayProxyHandler = async (event): Promise<APIGatewayProxyResult> => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: 'No body provided',
+    }
+  }
+
+  const customerId = event.pathParameters?.id
+
+  if (!customerId) {
+    return {
+      statusCode: 400,
+      body: 'No paath parameter provided',
+    }
+  }
+
+  const payload: HistoryRecord = JSON.parse(event.body)
+
+  const newHistoryRecord = new HistoryRecord(payload)
+
+  const errors = await validate(newHistoryRecord)
+
+  if (errors.length) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(errors),
+    }
+  }
+
+  const result = await CustomerService.createHistoryRecord(customerId, newHistoryRecord)
+
+  if (result) {
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(result),
+    }
+  } else {
+    return {
+      statusCode: 500,
+      body: 'Internal Server Error',
+    }
+  }
+}
